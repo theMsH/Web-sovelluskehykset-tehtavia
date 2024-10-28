7ee2bd174efb52f2c7e2688fd14920a5fb5ff36f
Miika Haapanen <123482448+theMsH@users.noreply.github.com>
2024-10-28 21:16:16 +0200

    TEHTÄVÄ 4: lisätty mongodb userille get_by_id metodi

--- repositories/users_mongo_repository.py
+++ repositories/users_mongo_repository.py
@@ -1,3 +1,5 @@
+from bson import ObjectId
+from werkzeug.exceptions import NotFound
 import models
 from repositories.users_repository import UsersRepository
 
@@ -11,23 +13,37 @@ https://juhaniguru-tiedonhallinta.onrender.com/nosql/#komentoja
 class UsersMongoRepository:
 
     def __init__(self, con):
-        self.con = con
+        # db on se tietokanta, johon päästään käsiksi clientissä, nimesin tietokannan mysql ja
+        # postgresql tietokantojen kaltaiseksi
+        # con = mongo client
+        self.db = con.sovelluskehykset_bad1
 
 
     def get_all(self):
-        # con = mongo client
-        with self.con as client:
-            # db on se tietokanta, johon päästään käsiksi clientissä, nimesin tietokannan mysql ja
-            # postgresql tietokantojen kaltaiseksi
-            db = client.sovelluskehykset_bad1
-            # kursori = tietokanta.kokoelma.kysely()
-            # Tuloksena on mongon cursor objekti, josta saadaan loopattua yksittäiset dictinonaryt
-            result = db.users.find()
+        # kursori = tietokanta.kokoelma.kysely()
+        # Tuloksena on mongon cursor objekti, josta saadaan loopattua yksittäiset dictinonaryt
+        result = self.db.users.find()
 
-            # mongo db palauttaa json datan suoraan
-            users = []
-            for user in result:
-                # Id on mongon ObjectId('671fb084ebc89b1017886382'), joka sisältää muitakin merkkejä kuin numeroita, joten se käsitellään str()
-                users.append(models.User(str(user['_id']), user['username'], user['firstname'], user['lastname']))
+        # mongo db palauttaa json datan suoraan
+        users = []
+        for user in result:
+            # Id on mongon ObjectId('671fb084ebc89b1017886382'), joka sisältää muitakin merkkejä kuin numeroita, joten se käsitellään str()
+            users.append(models.User(str(user['_id']), user['username'], user['firstname'], user['lastname']))
 
-            return users
+        return users
+
+
+    def get_by_id(self, user_oid):
+        # Tuloksena tulee lista, jossa on yksi user, kun haetaan ObjectId:llä.
+        # Tulokset pitää listauttaa .to_list() funktiolla, koska jos käyttäjää ei löydy,
+        # tulee "no such item for cursor instance" virhe.
+        # Lähde: https://stackoverflow.com/questions/78233990/no-such-item-for-cursor-instance
+        result = self.db.users.find( {"_id": ObjectId(user_oid)} ).to_list()
+
+        if not result:
+            raise NotFound('user not found')
+
+        user = result[0]
+        return models.User(str(user['_id']), user['username'], user['firstname'], user['lastname'])
+
+
